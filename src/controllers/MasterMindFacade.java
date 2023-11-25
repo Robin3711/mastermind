4a1d8cb9c3d19768401616dc3247f7862f542461
Antoine Cunin <cunin.antoine@sfr.fr>
2023-11-25 17:35:03 +0100

    Création et lancement de la fenêtre de menu - Création et lancement de la fenêtre de paramétrage - MAJ UML | UML à revoir

--- src/controllers/MasterMindFacade.java
+++ src/controllers/MasterMindFacade.java
@@ -1,5 +1,19 @@
 package controllers;
 
+import models.MasterMind;
+import views.SettingsWindowView;
+
 public class MasterMindFacade {
-    
+    private MasterMind masterMind;
+    public MasterMindFacade(MasterMind masterMind) {
+        this.masterMind = masterMind;
+    }
+
+    public void quitMasterMind() {
+        System.exit(0);
+    }
+
+    public void openSettingsWindow() {
+        SettingsWindowView settingsWindow = new SettingsWindowView(this);
+    }
 }
--- src/models/MasterMind.java
+++ src/models/MasterMind.java
@@ -1,5 +1,11 @@
 package models;
 
-public class MasterMind {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MasterMind {
+    private List<MasterMindObserver> ObserverList = new ArrayList<>();
+    public void addObserver(MasterMindObserver observer) {
+        ObserverList.add(observer);
+    }
 }
