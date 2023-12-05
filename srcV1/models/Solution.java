672bae7b57bcee83f5313972dcb396080263bc8e
Antoine Cunin <cunin.antoine@sfr.fr>
2023-12-05 08:42:50 +0100

    MAJ code source

--- srcV1/models/Solution.java
+++ srcV1/models/Solution.java
@@ -1,7 +1,9 @@
 package models;
 
 public class Solution extends Combination {
-    public Solution() {
+
+    public Solution(int nbColorsInCombination) {
+        super(nbColorsInCombination);
         generateSolution();
     }
 
@@ -45,5 +47,31 @@ public class Solution extends Combination {
             }
         }
         return true;
+    }
+
+    public Clue[] sortClues(Clue[] clues) {
+        Clue[] sortedClues = new Clue[getNbColorsInCombination()];
+        int nbWellPlaced = 0;
+        int nbMisplaced = 0;
+        int nbWrong = 0;
+        for (int i = 0; i < getNbColorsInCombination(); i++) {
+            if (clues[i] == Clue.WELL_PLACED) {
+                nbWellPlaced++;
+            } else if (clues[i] == Clue.MISPLACED) {
+                nbMisplaced++;
+            } else {
+                nbWrong++;
+            }
+        }
+        for (int i = 0; i < nbWellPlaced; i++) {
+            sortedClues[i] = Clue.WELL_PLACED;
+        }
+        for (int i = nbWellPlaced; i < nbWellPlaced + nbMisplaced; i++) {
+            sortedClues[i] = Clue.MISPLACED;
+        }
+        for (int i = nbWellPlaced + nbMisplaced; i < nbWellPlaced + nbMisplaced + nbWrong; i++) {
+            sortedClues[i] = Clue.WRONG;
+        }
+        return sortedClues;
     }
 }
