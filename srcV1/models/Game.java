add33b51e90ca0eb5e73f7b47faf8890c1ebd70b
miniotti <robin.miniotti@etu.unistra.fr>
2024-01-05 16:57:58 +0100

    réparé(?) les rounds

--- srcV1/models/Game.java
+++ srcV1/models/Game.java
@@ -8,52 +8,30 @@ import java.util.List;
 public class Game {
     private Round[] _rounds;
     private List<GameObserver> _observers;
+    private int _currentRound;
 
     public Game(int nbRounds) {
         System.out.println("Game created");
         this._rounds = new Round[nbRounds];
         _observers = new ArrayList<>();
+        _currentRound = 0;
     }
 
-    public void nextRound(int nbAttempts, int nbColorsInCombination, GameMode gameMode)
-    {
-        int currentRoundIndex = getCurrentRoundNb();
-        System.out.println("Round " + (currentRoundIndex) + " launched");
-        _rounds[currentRoundIndex] = new Round(nbAttempts, nbColorsInCombination, gameMode);
+    public void nextRound(int nbAttempts, int nbColorsInCombination, GameMode gameMode) {
+        System.out.println("Round " + (_currentRound + 1) + " launched");
+        _rounds[_currentRound] = new Round(nbAttempts, nbColorsInCombination, gameMode);
+        _currentRound++;
     }
-
 
     public Round getCurrentRound() {
-        return _rounds[getCurrentRoundNb()];
+        return _rounds[_currentRound - 1];
     }
-
-    private int getCurrentRoundNb()
-    {
-        if(_rounds[0] == null)
-        {
-            System.out.println("aucun round ?!");
-            return 0;
-        }
-        for(int i = 1; i < _rounds.length; i++)
-        {
-            if(_rounds[i]==null)
-            {
-                System.out.println("vous etes au round"+ (i-1));
-                return i -1;
-            }
-        }
-
-        System.out.println("vous etes au dernier round, le round "+ (_rounds.length-1));
-        return _rounds.length-1;
-    }
-
 
     private void endGame() {
         return;
     }
 
-    public int CalculateScoreGame()
-    {
+    public int CalculateScoreGame() {
         int score = 0;
         for (Round round : _rounds) {
             score += round.calculateScoreRound();
@@ -66,53 +44,47 @@ public class Game {
     }
 
     private void notifyAttemptPerformed(Attempt attempt) {
-        for (GameObserver gameObserver: _observers) {
+        for (GameObserver gameObserver : _observers) {
             gameObserver.onAttemptPerformed(attempt);
         }
     }
 
     private void notifyRoundFinished() {
-        for (GameObserver gameObserver: _observers) {
+        for (GameObserver gameObserver : _observers) {
             gameObserver.onRoundFinished();
         }
     }
 
-    public void submitCombination(Combination combination)
-    {
+    public void submitCombination(Combination combination) {
         Round currentRound = getCurrentRound();
         Attempt attempt = currentRound.submitCombination(combination);
         notifyAttemptPerformed(attempt);
-        if (currentRound.isRoundOver())
-        {
+        if (currentRound.isRoundOver()) {
             /*if (isGameOver()) {
                 notifyGameFinished();
             } else {*/
-                notifyRoundFinished();
+            notifyRoundFinished();
         }
     }
 
     private void notifyGameFinished() {
-        for (GameObserver gameObserver: _observers) {
+        for (GameObserver gameObserver : _observers) {
             gameObserver.onGameFinished();
         }
     }
 
-    public int getNbRoundsWon()
-    {
+    public int getNbRoundsWon() {
         int nbRoundsWon = 0;
 
-        for(int i = 0; i < _rounds.length; i++)
-        {
-            if(_rounds[i].getIsWon())
-            {
+        for (int i = 0; i < _currentRound; i++) {
+            if (_rounds[i].getIsWon()) {
                 nbRoundsWon++;
             }
         }
         return nbRoundsWon;
     }
 
-    public boolean isGameOver()
-    {
-        return (getCurrentRoundNb() == _rounds.length-1 && getCurrentRound().isRoundOver());
+    public boolean isGameOver() {
+        return (_currentRound == _rounds.length && getCurrentRound().isRoundOver());
     }
 }
