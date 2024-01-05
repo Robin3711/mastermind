70dab1928abe848007ac78f62e6d9645c51c7502
miniotti <robin.miniotti@etu.unistra.fr>
2024-01-05 11:14:50 +0100

    j'ai merge ( manuellement ) nos deux branches

--- srcV1/models/Solution.java
+++ srcV1/models/Solution.java
@@ -1,5 +1,8 @@
 package models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution extends Combination {
 
     public Solution(int nbColorsInCombination) {
@@ -12,32 +15,41 @@ public class Solution extends Combination {
         for (int i = 0; i < getNbColorsInCombination(); i++) {
             _pawns[i] = PawnColor.values()[(int) (Math.random() * PawnColor.values().length)];
         }
+        // Display solution
+        System.out.print("Solution: ");
+        for (int i = 0; i < getNbColorsInCombination(); i++) {
+            System.out.print(_pawns[i] + " ");
+        }
     }
 
     public Clue[] compareWithCombination(Combination combination) {
         // If a pawn is at the same place in the solution and in the combination, it's well placed
         // If a pawn is in the solution but not at the same place in the combination, it's misplaced
         // If a pawn is not in the solution, it's wrong
-        Clue[] clues = new Clue[getNbColorsInCombination()];
-        for (int i = 0; i < getNbColorsInCombination(); i++) {
+        int nbColorsInCombination = getNbColorsInCombination();
+        Clue[] clues = new Clue[nbColorsInCombination];
+        HashMap<PawnColor, Integer> nbOccurencesInSolution = new HashMap<>();
+
+        for (int i = 0; i < nbColorsInCombination; i++) {
             if (_pawns[i] == combination._pawns[i]) {
                 clues[i] = Clue.WELL_PLACED;
-            } else if (isInSolution(combination._pawns[i])) {
-                clues[i] = Clue.MISPLACED;
             } else {
-                clues[i] = Clue.WRONG;
+                nbOccurencesInSolution.put(_pawns[i], nbOccurencesInSolution.getOrDefault(_pawns[i], 0) + 1);
             }
         }
-        return clues;
-    }
 
-    private boolean isInSolution(PawnColor pawnColor) {
-        for (int i = 0; i < getNbColorsInCombination(); i++) {
-            if (_pawns[i] == pawnColor) {
-                return true;
+        for (int i = 0; i < nbColorsInCombination; i++) {
+            if (clues[i] == null) {
+                if (nbOccurencesInSolution.getOrDefault(combination._pawns[i], 0) > 0) {
+                    clues[i] = Clue.MISPLACED;
+                    nbOccurencesInSolution.put(combination._pawns[i], nbOccurencesInSolution.get(combination._pawns[i]) - 1);
+                } else {
+                    clues[i] = Clue.WRONG;
+                }
             }
         }
-        return false;
+
+        return clues;
     }
 
     public boolean isSolutionFound(Clue[] clues) {
@@ -47,31 +59,5 @@ public class Solution extends Combination {
             }
         }
         return true;
-    }
-
-    public Clue[] sortClues(Clue[] clues) {
-        Clue[] sortedClues = new Clue[getNbColorsInCombination()];
-        int nbWellPlaced = 0;
-        int nbMisplaced = 0;
-        int nbWrong = 0;
-        for (int i = 0; i < getNbColorsInCombination(); i++) {
-            if (clues[i] == Clue.WELL_PLACED) {
-                nbWellPlaced++;
-            } else if (clues[i] == Clue.MISPLACED) {
-                nbMisplaced++;
-            } else {
-                nbWrong++;
-            }
-        }
-        for (int i = 0; i < nbWellPlaced; i++) {
-            sortedClues[i] = Clue.WELL_PLACED;
-        }
-        for (int i = nbWellPlaced; i < nbWellPlaced + nbMisplaced; i++) {
-            sortedClues[i] = Clue.MISPLACED;
-        }
-        for (int i = nbWellPlaced + nbMisplaced; i < nbWellPlaced + nbMisplaced + nbWrong; i++) {
-            sortedClues[i] = Clue.WRONG;
-        }
-        return sortedClues;
     }
 }
